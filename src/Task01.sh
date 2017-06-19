702e747b415a57e27483dccffcc79280a50c0c69
Alexander Radchenko <partner.ar@gmail.com>
2017-06-19 05:58:04 +0300

    Deleted Task0x.sh files, where x>1. Edited README.md

--- src/Task01.sh
+++ src/Task01.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#Assuming we run this script from the directory, where javase01 folder is.
+#Assuming we run this script from the directory, where /main/ folder is.
 
-javac main/java/javase01/t01/main/Main.java
-
-#TODO: проверить на убунту при такой структуре папок
-java javase01.t01.main.Main
+javac -cp .\main\java\ main/java/javase01/t01/main/Main.java
+java -cp .\main\java\ javase01.t01.main.Main
--- src/Task02.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Assuming we run this script from the directory, where javase01 folder is.
-
-javac main/java/javase01/t01/Task02.java
-java javase01.t01.Task02 0.003
