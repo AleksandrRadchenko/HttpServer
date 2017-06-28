42f98aa4e4b3801a4ef59cc26f053d1ce6d4de1b
Alexander Radchenko <partner.ar@gmail.com>
2017-06-29 01:49:22 +0300

    Started javase02, before trying to apply gradle module structure

---
+++ src/main/java/javase02/t01/Pen.java
@@ -0,0 +1,32 @@
+package javase02.t01;
+
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+public class Pen {
+    private String name;
+    private BigDecimal price;
+
+    /**
+     * Creates new Pen with default name = "Pen" and price = 1.00;
+     */
+    public Pen() {
+        setName("Pen");
+        setPrice(new BigDecimal("1.00"));
+    }
+
+    /**
+     * Creates new Pen with default price = 1.00;
+     * @param name
+     */
+    public Pen(String name) {
+        setName(name);
+        setPrice(new BigDecimal("1.00"));
+    }
+
+    public Pen(String name, BigDecimal price) {
+        setName(name);
+        setPrice(price);
+    }
+}
