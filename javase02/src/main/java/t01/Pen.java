3fbd2ed41941af9899e21ba006d1ee336bb086b6
Alexander Radchenko <partner.ar@gmail.com>
2017-07-02 19:38:36 +0300

    Task01 (Pen()) done. Tests done.

--- javase02/src/main/java/t01/Pen.java
+++ javase02/src/main/java/t01/Pen.java
@@ -8,31 +8,4 @@ import java.math.BigDecimal;
 public class Pen {
     private String name;
     private BigDecimal price;
-
-//    /**
-//     * Creates new Pen with default name = "Pen" and price = 1.00;
-//     */
-//    public Pen() {
-//        this.name = "Pen";
-//        this.price = new BigDecimal("1.00");
-//    }
-//
-//    /**
-//     * Creates new Pen with default price = 1.00;
-//     * @param name
-//     */
-//    public Pen(String name) {
-//        this.name = name;
-//        this.price = new BigDecimal("1.00");
-//    }
-//
-//    /**
-//     * Creates new Pen with default name = "Pen";
-//     * @param price
-//     */
-//    public Pen(BigDecimal price) {
-//        this.name = "Pen";
-//        this.price = new BigDecimal("1.00");
-//    }
-
 }
