dd4f09074c05e1d6a3c160aae45dc6edb3856a69
Alexander Radchenko <partner.ar@gmail.com>
2017-07-02 19:32:29 +0300

    Created class Pen and all constructors. Will delete constructors later

--- javase02/src/main/java/t01/Pen.java
+++ javase02/src/main/java/t01/Pen.java
@@ -1,32 +1,38 @@
 package t01;
 
-import lombok.Data;
+import lombok.*;
+
 import java.math.BigDecimal;
 
-@Data
+@Value
 public class Pen {
     private String name;
     private BigDecimal price;
 
-    /**
-     * Creates new Pen with default name = "Pen" and price = 1.00;
-     */
-    public Pen() {
-        setName("Pen");
-        setPrice(new BigDecimal("1.00"));
-    }
+//    /**
+//     * Creates new Pen with default name = "Pen" and price = 1.00;
+//     */
+//    public Pen() {
+//        this.name = "Pen";
+//        this.price = new BigDecimal("1.00");
+//    }
+//
+//    /**
+//     * Creates new Pen with default price = 1.00;
+//     * @param name
+//     */
+//    public Pen(String name) {
+//        this.name = name;
+//        this.price = new BigDecimal("1.00");
+//    }
+//
+//    /**
+//     * Creates new Pen with default name = "Pen";
+//     * @param price
+//     */
+//    public Pen(BigDecimal price) {
+//        this.name = "Pen";
+//        this.price = new BigDecimal("1.00");
+//    }
 
-    /**
-     * Creates new Pen with default price = 1.00;
-     * @param name
-     */
-    public Pen(String name) {
-        setName(name);
-        setPrice(new BigDecimal("1.00"));
-    }
-
-    public Pen(String name, BigDecimal price) {
-        setName(name);
-        setPrice(price);
-    }
 }
