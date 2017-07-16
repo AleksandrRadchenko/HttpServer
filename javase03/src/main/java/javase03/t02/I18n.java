2d188e7f7431f0c3788a2ca77857d7de14442c84
Alexander Radchenko <partner.ar@gmail.com>
2017-07-16 22:31:00 +0300

    ResourceBundle encoding via UTF8Control (extends Control) class

--- javase03/src/main/java/javase03/t02/I18n.java
+++ javase03/src/main/java/javase03/t02/I18n.java
@@ -13,13 +13,29 @@ public class I18n {
     public static void main(String[] args) {
         I18n i18n = new I18n();
         i18n.readUserInput();
+// Default locale for testing
+//        i18n.currentLocale = new Locale("ru");
+
+//  --- Block where encoding is chosen in UTF8Control class. ---
         if ("ru".equals(i18n.userInput)) i18n.currentLocale = new Locale("ru");
         if ("en".equals(i18n.userInput)) i18n.currentLocale = new Locale("en");
-        ResourceBundle questions = ResourceBundle.getBundle("Questions", i18n.currentLocale);
+        ResourceBundle questions = ResourceBundle.getBundle("t02.I18n.Questions", i18n.currentLocale, new UTF8Control());
         for (String q : questions.keySet()) {
-            System.out.println(q);
+            System.out.println(questions.getString(q));
         }
+//  --- Block where encoding is chosen in UTF8Control class. ---
 
+
+//  --- Block where encoding is chosen using getBytes method and String constructor; ---
+//        ResourceBundle questions = ResourceBundle.getBundle("t02.I18n.Questions", i18n.currentLocale);
+//        try {
+//            for (String q : questions.keySet()) {
+//                System.out.println(new String(questions.getString(q).getBytes("ISO-8859-1"), "UTF-8"));
+//            }
+//        } catch (UnsupportedEncodingException e) {
+//            System.out.println(e);
+//        }
+//  --- Block where encoding is chosen using getBytes method and String constructor; ---
     }
 
     private void readUserInput() {
