9f920c3431e638f02d8c6028ccc4a21eae863cc9
Alexander Radchenko <partner.ar@gmail.com>
2017-07-17 18:58:43 +0300

    javase03.t02 done (ResourceBundle). Without tests.

--- javase03/src/main/java/javase03/t02/I18n.java
+++ javase03/src/main/java/javase03/t02/I18n.java
@@ -4,48 +4,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class I18n {
     private String userInput = "";
-    Locale currentLocale = Locale.getDefault();
+    private Locale currentLocale = Locale.getDefault();
 
     public static void main(String[] args) {
         I18n i18n = new I18n();
-        i18n.readUserInput();
-// Default locale for testing
-//        i18n.currentLocale = new Locale("ru");
-
-//  --- Block where encoding is chosen in UTF8Control class. ---
-        if ("ru".equals(i18n.userInput)) i18n.currentLocale = new Locale("ru");
-        if ("en".equals(i18n.userInput)) i18n.currentLocale = new Locale("en");
-        ResourceBundle questions = ResourceBundle.getBundle("t02.I18n.Questions", i18n.currentLocale, new UTF8Control());
-        for (String q : questions.keySet()) {
-            System.out.println(questions.getString(q));
-        }
-//  --- Block where encoding is chosen in UTF8Control class. ---
-
-
-//  --- Block where encoding is chosen using getBytes method and String constructor; ---
-//        ResourceBundle questions = ResourceBundle.getBundle("t02.I18n.Questions", i18n.currentLocale);
-//        try {
-//            for (String q : questions.keySet()) {
-//                System.out.println(new String(questions.getString(q).getBytes("ISO-8859-1"), "UTF-8"));
-//            }
-//        } catch (UnsupportedEncodingException e) {
-//            System.out.println(e);
-//        }
-//  --- Block where encoding is chosen using getBytes method and String constructor; ---
-    }
-
-    private void readUserInput() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            while (!"en".equals(userInput) && !"ru".equals(userInput) && !"s".equals(userInput)) {
+            // Reading language
+            while (!"en".equals(i18n.userInput) && !"ru".equals(i18n.userInput) && !"s".equals(i18n.userInput)) {
                 System.out.print("Please, choose language ('en' for English, 'ru' for Russian, 's' to stop): ");
-                userInput = br.readLine();
+                i18n.userInput = br.readLine();
             }
-        } catch (IOException e) {
-            System.out.println(e);
+            if ("ru".equals(i18n.userInput)) i18n.currentLocale = new Locale("ru");
+            if ("en".equals(i18n.userInput)) i18n.currentLocale = new Locale("en");
+
+            //  --- Block where encoding is chosen using getBytes method and String constructor; ---
+            ResourceBundle questions = ResourceBundle.getBundle("t02.I18n.Questions", i18n.currentLocale);
+            for (String q : questions.keySet()) {
+//                if (!q.equals("q0")) System.out.println(i18n.getResourceInUTF(questions, q));
+                if (!q.equals("qNumberPrompt")) System.out.println(questions.getString(q));
+            }
+            //  --- Block where encoding is chosen using getBytes method and String constructor; ---
+
+            // Reading question number
+            i18n.userInput = "";
+            int userInputInt = 0;
+            while (!(userInputInt > 0 && userInputInt < questions.keySet().size())) {
+                System.out.print(questions.getString("qNumberPrompt"));
+                i18n.userInput = br.readLine();
+                userInputInt = Integer.parseInt(i18n.userInput);
+            }
+
+            // Printing answer
+            ResourceBundle answers = ResourceBundle.getBundle("t02.I18n.Answers", i18n.currentLocale);
+            System.out.println(answers.getString("a" + userInputInt));
+
+        } catch (MissingResourceException | IOException | NumberFormatException exception) {
+            System.out.println(exception);
         }
     }
 }
