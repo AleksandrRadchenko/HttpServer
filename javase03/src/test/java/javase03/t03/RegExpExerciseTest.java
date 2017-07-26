add0671ddb3f98ea7c4ef2d66da04fee9ca272ed
Alexander Radchenko <partner.ar@gmail.com>
2017-07-26 03:05:28 +0300

    Regex: repaired tests

--- javase03/src/test/java/javase03/t03/RegExpExerciseTest.java
+++ javase03/src/test/java/javase03/t03/RegExpExerciseTest.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RegExpExerciseTest {
-    private static final String fileName = "javase03/src/main/resources/t03/PicLinksFinder/JF03 - 3.1 - Information handling_task_attachment.html";
+    private static final String fileName = "src/main/resources/t03/PicLinksFinder/JF03 - 3.1 - Information handling_task_attachment.html";
     private static PicLinksFinder finder = new PicLinksFinder();
     private static List<String> sentences;
     private List<String> testText = new ArrayList<>();
@@ -36,7 +36,7 @@ public class RegExpExerciseTest {
         testText.add("        В электроне (позитроне) гамма-коллапсары образуют семь гамма-трубок (Рис. 3), в каждой из семи гамма-трубок по семь элементарных Рисунке 4 и рисунка 5 трубок, всего в (Рис. 15,16) (Рис. 25 и 26) электроне (позитроне) 49 элементарных трубок, сорок девятая трубка расположена в центре.".trim());
         testText.add("                Новое описание истинной структуры ядра атома углерода основано на универсальных свойствах коллапсаров-нуклонов в силовых структурах трёх альф, расположенных в ядерной трубке атома углерода с взаимным относительным смещением на 60 (рис. 27).".trim());
         testText.add("        Только мощные свободные неэлектростатические – спиновые (сторонние) электрические заряды протонов, в ядрах атома углерода создают силовую кристаллическую структуру графита (Рис. 28), фуллерена (Рис. 29) и алмаза (Рис. 30).".trim());
-        testText.add("   рисунках 17 и 20  рисунки 21 и 22 рисунков 37, 40".trim());
+        testText.add("   рисунках 31 и 32 рисунки 33 и 34 рисунков 37, 40".trim());
     }
 
     @Test
@@ -54,7 +54,7 @@ public class RegExpExerciseTest {
         List<Integer> string0Links = new ArrayList<>(Arrays.asList(3, 4, 5, 15, 16, 25, 26));
         List<Integer> string1Links = new ArrayList<>(Arrays.asList(27));
         List<Integer> string2Links = new ArrayList<>(Arrays.asList(28, 29, 30));
-        List<Integer> string3Links = new ArrayList<>(Arrays.asList(17, 20, 21, 22, 37, 40));
+        List<Integer> string3Links = new ArrayList<>(Arrays.asList(31, 32, 33, 34, 37, 40));
         printPicLinks(picLinks);
         assertThat(picLinks.get(0)._2, Is.is(string0Links));
         assertThat(picLinks.get(1)._2, Is.is(string1Links));
