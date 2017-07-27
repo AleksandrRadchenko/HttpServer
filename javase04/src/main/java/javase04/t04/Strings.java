c28470b53accbd22725c98b787b0f70a9a3ab930
Alexander Radchenko <partner.ar@gmail.com>
2017-07-27 05:48:57 +0300

    javase04.t04 trying another path for serialization - (from javase04/src...). Local build failed.

--- javase04/src/main/java/javase04/t04/Strings.java
+++ javase04/src/main/java/javase04/t04/Strings.java
@@ -2,7 +2,7 @@ package javase04.t04;
 
 public interface Strings {
 //    String FILE_PATH = "";
-    String FILE_PATH = "src/main/resources/javase04/t04/";
-//    String FILE_PATH = "javase04/src/main/resources/javase04/t04/";
+//    String FILE_PATH = "src/main/resources/javase04/t04/";
+    String FILE_PATH = "javase04/src/main/resources/javase04/t04/";
     String FILE_NAME = "movieCollection.ser";
 }
