6ddce52e5e128fbf3d0f24378b953636c7955e25
Alexander Radchenko <partner.ar@gmail.com>
2017-07-27 04:49:23 +0300

    javase04.t04 due to failed build, removed path to file

--- javase04/src/main/java/javase04/t04/Strings.java
+++ javase04/src/main/java/javase04/t04/Strings.java
@@ -1,7 +1,8 @@
 package javase04.t04;
 
 public interface Strings {
-    String FILE_PATH = "src/main/resources/javase04/t04/";
+    String FILE_PATH = "";
+//    String FILE_PATH = "src/main/resources/javase04/t04/";
 //    String FILE_PATH = "javase04/src/main/resources/javase04/t04/";
     String FILE_NAME = "movieCollection.ser";
 }
