03a406ca86d01e769ba65956cca974536f68772a
Alexander Radchenko <partner.ar@gmail.com>
2017-08-01 13:32:29 +0300

    javase06.t01 - added more collections

--- javase06/src/main/java/javase06/t01/CollClassesTable.java
+++ javase06/src/main/java/javase06/t01/CollClassesTable.java
@@ -1,9 +1,6 @@
 package javase06.t01;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CollClassesTable {
     public static void main(String[] args) {
@@ -14,12 +11,18 @@ public class CollClassesTable {
         System.out.println("               Ordering   |   Random   | Key-Value  |   Allows   | Allows Null|   Thread   |  Blocking ");
         System.out.println("                          |   Access   |   Pairs    | Duplicates |    Values  |    Safe    | Operations");
         System.out.println("-------------------------------------------------------------------------------------------------------------");
-        Map<String, List<Integer>> table = new HashMap<>();
+        Map<String, List<Integer>> table = new LinkedHashMap<>();
 
-        table.put("ArrayList ", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
-        table.put("LinkedList", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
-        table.put("Vector    ", Arrays.asList(1, 1, 0, 1, 1, 1, 0));
-        table.put("Stack     ", Arrays.asList(1, 0, 0, 1, 1, 1, 0));
+        table.put("ArrayList    ", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
+        table.put("LinkedList   ", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
+        table.put("Vector       ", Arrays.asList(1, 1, 0, 1, 1, 1, 0));
+        table.put("Stack        ", Arrays.asList(1, 0, 0, 1, 1, 1, 0));
+        table.put("HashMap      ", Arrays.asList(0, 1, 1, 0, 1, 0, 0));
+        table.put("TreeMap      ", Arrays.asList(1, 1, 1, 0, 1, 0, 0));
+        table.put("LinkedHashMap", Arrays.asList(1, 1, 1, 0, 1, 0, 0));
+        table.put("HashSet      ", Arrays.asList(0, 1, 0, 0, 1, 0, 0));
+        table.put("TreeSet      ", Arrays.asList(1, 1, 0, 0, 1, 0, 0));
+        table.put("LinkedHashSet", Arrays.asList(0, 1, 0, 0, 1, 0, 0));
 
         for (String s : table.keySet()) {
             System.out.printf(s);
