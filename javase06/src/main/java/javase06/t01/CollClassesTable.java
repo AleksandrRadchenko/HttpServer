eb97533045f76abc2d0d25d502b75cd62e4b4ed0
Alexander Radchenko <partner.ar@gmail.com>
2017-08-01 03:56:25 +0300

    javase06.t01 - created table, added few collections

--- javase06/src/main/java/javase06/t01/CollClassesTable.java
+++ javase06/src/main/java/javase06/t01/CollClassesTable.java
@@ -1,5 +1,6 @@
 package javase06.t01;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,15 +11,20 @@ public class CollClassesTable {
     }
 
     private static void printCollectionsTable() {
-        System.out.println("                 Ordering   |   Random    | Key-Value  |   Allows    | Allows Null |   Thread   |  Blocking ");
-        System.out.println("                            |   Access    |   Pairs    | Duplicates  |    Values   |    Safe    | Operations");
+        System.out.println("               Ordering   |   Random   | Key-Value  |   Allows   | Allows Null|   Thread   |  Blocking ");
+        System.out.println("                          |   Access   |   Pairs    | Duplicates |    Values  |    Safe    | Operations");
         System.out.println("-------------------------------------------------------------------------------------------------------------");
         Map<String, List<Integer>> table = new HashMap<>();
+
+        table.put("ArrayList ", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
+        table.put("LinkedList", Arrays.asList(1, 1, 0, 1, 1, 0, 0));
+        table.put("Vector    ", Arrays.asList(1, 1, 0, 1, 1, 1, 0));
+        table.put("Stack     ", Arrays.asList(1, 0, 0, 1, 1, 1, 0));
 
         for (String s : table.keySet()) {
             System.out.printf(s);
             for (Integer i : table.get(s)) {
-                System.out.printf("    |     %s ", (i == 1 ? "Yes" : "No"));
+                System.out.printf("   |     %s ", (i == 1 ? "Yes" : "No "));
             }
             System.out.println();
         }
