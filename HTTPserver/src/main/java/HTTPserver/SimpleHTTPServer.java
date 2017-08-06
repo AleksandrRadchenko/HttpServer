79fea2d47876fc3dcc6851925add2a57e7167a59
Alexander Radchenko <partner.ar@gmail.com>
2017-08-07 00:25:58 +0300

    HTTP server - added tests, find local ip

--- HTTPserver/src/main/java/HTTPserver/SimpleHTTPServer.java
+++ HTTPserver/src/main/java/HTTPserver/SimpleHTTPServer.java
@@ -1,6 +1,16 @@
 package HTTPserver;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
+
+import java.io.IOException;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Log4j2
 public class SimpleHTTPServer {
@@ -10,21 +20,58 @@ public class SimpleHTTPServer {
      *             For example: "8080"
      * @return 1 if all ok, -1 if cant' parse for port number, 0 if no args provided
      */
+    private ServerSocket ss;
     public int openPort(String[] args) {
         int result = 0;
-        if (args.length < 1) {
+        if ((args == null) || (args.length < 1)) {
             log.info("Port to listen not specified");
             result = 0;
         } else {
             try {
                 int port = Integer.parseInt(args[0]);
-                log.info("specified port = " + port);
-                result = 1;
+                if (port < 1025 || port > 65535) {
+                    log.error("Port should be in range 1025-65535");
+                    result = 0;
+                } else {
+                    log.info("specified port = " + port);
+                    result = 1;
+                    ss = new ServerSocket(port); // Starting server
+                    log.printf(Level.INFO, "Server started: http:/%s:%d", getLocalIpAddr(), port);
+                }
             } catch (NumberFormatException e) {
                 log.error("can't parse " + e.getMessage() + ". Please specify correct port.");
                 result = -1;
+            } catch (IOException e) {
+                log.error("can't start " + e.getMessage() + ". Please specify correct port.");
+
+            } finally {
+                try {
+                    if (ss != null) ss.close();
+                } catch (IOException e) {
+                    log.error("Error while closing server socket", e);
+                }
             }
         }
         return result;
+    }
+
+    @SneakyThrows
+    /**
+     * Returns first fount ip addres, which contains with "192" or "10.0"
+     */
+    private static String getLocalIpAddr() {
+        Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+        List<InterfaceAddress> addrList = new ArrayList<>();
+        while (ifaces.hasMoreElements()) {
+            addrList.addAll(ifaces.nextElement().getInterfaceAddresses());
+        }
+        // Getting first local addr
+        String localAddr = "";
+        for (InterfaceAddress interfaceAddress : addrList) {
+            localAddr = interfaceAddress.getAddress().toString();
+            if (localAddr.contains("192") || localAddr.contains("10.0"))
+                break;
+        }
+        return localAddr;
     }
 }
--- HTTPserver/src/test/java/HTTPserver/SimpleHTTPServerTest.java
+++ HTTPserver/src/test/java/HTTPserver/SimpleHTTPServerTest.java
@@ -9,10 +9,33 @@ public class SimpleHTTPServerTest {
     SimpleHTTPServer server = new SimpleHTTPServer();
 
     @Test
-    public void portNumShouldBeIntegerString() throws Exception {
+    public void strArgsFail() throws Exception {
         String[] args = {"asd"};
         int expected = server.openPort(args);
         assertThat(expected, Is.is(-1));
+    }
+
+    @Test
+    public void portWrongRangeFail() throws Exception {
+        String[] args = {"80"};
+        int expected = server.openPort(args);
+        assertThat(expected, Is.is(0));
+    }
+
+    @Test
+    public void noArgsFail() throws Exception {
+        String[] args = {};
+        int expected = server.openPort(args);
+        assertThat(expected, Is.is(0));
+        args = null;
+        expected = server.openPort(args);
+        assertThat(expected, Is.is(0));
+    }
+
+    @Test
+    public void test() throws Exception {
+        String[] args = {"8080"};
+        int expected = server.openPort(args);
     }
 
 }
