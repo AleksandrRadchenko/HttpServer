4b88bd9b4152cb310bcc00d3c371aea97403036f
Alexander Radchenko <partner.ar@gmail.com>
2017-08-17 14:11:53 +0300

    HTTP server - added test to check data integrity for txt file

--- HTTPserver/src/test/java/HTTPserver/SimpleHTTPServerTest.java
+++ HTTPserver/src/test/java/HTTPserver/SimpleHTTPServerTest.java
@@ -3,11 +3,10 @@ package HTTPserver;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static org.junit.Assert.assertThat;
 
@@ -39,20 +38,53 @@ public class SimpleHTTPServerTest {
     }
 
     @Test
-    public void test() throws Exception {
-        ExecutorService executorService = Executors.newCachedThreadPool();
+    public void test0() throws Exception {
         String[] args = {"8080"};
-        executorService.execute(new ServerStarter(args));
-//        int expected = server.openPort(args);
-        Socket socket = new Socket("192.168.0.79", 8080);
-        InputStream is = socket.getInputStream();
-        OutputStream os = socket.getOutputStream();
-        os.write("GET /face4small4.jpg HTTP/1.1\r\nHost: http://192.168.0.79:8080".getBytes());
-        while (is.available() > 0) {
-            System.out.print(is.read());
+        int expected = server.openPort(args);
+    }
+
+    @Test
+    public void test1() throws Exception {
+        String[] args = {"8080"};
+        Thread t1 = new Thread(new ServerStarter(args));
+        t1.start();
+
+        String urlString = "http://localhost:8080/test.txt";
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+//        System.out.println("\nSending 'GET' request to URL : " + urlString);
+//        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+            response.append(System.lineSeparator());
         }
-        System.out.println("End of stream");
-        socket.close();
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+        //testing for equivalence with original file
+        byte[] actual = response.toString().getBytes();
+        byte[] expected = FileProcessor.getFile(Strings.PATH + "/test.txt");
+        assertThat(actual, Is.is(expected));
+        t1.interrupt();
+//        t1.join();
+
+//        InputStream is = socket.getInputStream();
+//        OutputStream os = socket.getOutputStream();
+//        os.write("GET /face4small4.jpg HTTP/1.1\r\nHost: http://192.168.0.79:8080".getBytes());
+//        while (is.available() > 0) {
+//            System.out.print(is.read());
+//        }
+//        System.out.println("End of stream");
+//        socket.close();
     }
 
 }
